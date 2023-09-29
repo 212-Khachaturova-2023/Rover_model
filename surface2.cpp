18c003ae837fe47e142b5ecd63ffe901bb091b58
212-Khachaturova-2023 <145916604+212-Khachaturova-2023@users.noreply.github.com>
2023-09-29 18:41:26 +0300

    Update surface2.cpp

--- surface2.cpp
+++ surface2.cpp
@@ -1,63 +1,75 @@
-﻿#include <iostream>
+#include <iostream>
 #include <stdlib.h>
 #include <time.h>
 #include <string>
 #include <fstream>
 #include <math.h>
 
+#define NUM 25
 //Хачатурова Ирина
 //group 212
 using namespace std;
 
-//class for points on surface
 class point {
 protected:
-    double x, y, z; //x, y, z-coordinates
-    double sig_x = 0.5, sig_y = 0.5; //x, y-variance
+    double x, y, z;
 public:
-    //class constructor
-    point(double xc = 0.0, double yc = 0.0, double zc = 0.0) {
-        x = xc;
-        y = yc;
-        z = zc;
+    //class point constructor
+    point(double xc = 0, double yc = 0, double zc = 0) {
+        x = xc, y = yc, z = zc;
     }
 };
 
+//class hill is a subclass of class point
+class hill : public point {
+    friend class surface;
+private:
+    double sig_x, sig_y; //x, y - variance
+    double var;
+    string func;
+public:
+    hill(double xx = 0, double yy = 0, double zz = 0, double sig_xx = 0.5, double sig_yy = 0.5, double alpha = 0);
+};
+
+hill::hill(double xx, double yy, double zz, double sig_xx, double sig_yy, double alpha) {
+    x = xx, y = yy, z = zz;
+    sig_x = sig_xx;
+    sig_y = sig_yy;
+    func = "(" + to_string(z) + "*" + "exp(" + "(-1)*((x-(" + to_string(x) + "))**2" + "/" + to_string(sig_x) + " + " + "((y-(" + to_string(y) + "))**2)" + "/" + to_string(sig_y) + ")))";
+}
+
 class surface {
-    friend class stone;
 private:
     double dim_x = 25.0, dim_y = 25.0;
-    int count = 40;
-    string func = "splot ";
+    double variance = 0.5;
+
+    hill* points[NUM];
 public:
-    surface(double length = 25.0, double width = 25.0, int num = 40);
+    surface(ofstream & file, double length = 25.0, double width = 25.0, double var = 0.5);
+    double h(double x, double y);
     void print_file(ofstream& file);
 };
 
-//function printing formula to the file
-void surface::print_file(ofstream& file) {
-    file << func;
-}
-
-surface::surface(double length, double width, int num) {
+surface::surface(ofstream & file, double length, double width, double var) {
     double dim_x = length;
     double dim_y = width;
-    int count = num;
-    for (int i = 0; i < count; i++) {
+    int variance = var;
+    for (int i = 0; i < NUM; i++) {
         double x0 = rand() / double(RAND_MAX) * dim_x - dim_x / 2;
         double y0 = rand() / double(RAND_MAX) * dim_y - dim_y / 2;
+
         //heigth of bells between -5 and 5
         double z0 = rand() / double(RAND_MAX) * 10 - 5;
 
         double sig_x0 = rand() / double(RAND_MAX) * 2.9 + 0.1;
         double sig_y0 = rand() / double(RAND_MAX) * 2.9 + 0.1;
 
-        if (i != count - 1) {
-            func += to_string(z0) + '*' + "exp(-((x -" + to_string(x0) + ")**2 / (2 * " + to_string(sig_x0) + "**2) + " + "(y -" + to_string(y0) + ")**2 / (2 * " + to_string(sig_y0) + "**2))) + ";
-        }
-        else {
-            func += to_string(z0) + '*' + "exp(-((x -" + to_string(x0) + ")**2 / (2 * " + to_string(sig_x0) + "**2) + " + "(y -" + to_string(y0) + ")**2 / (2 * " + to_string(sig_y0) + "**2)))";
-        }
+        double var_0 = rand() / double(RAND_MAX) * 10;
+
+        //adding raandomly generated points to the array with objects from class hill
+        points[i] = new hill(x0, y0, z0, sig_x0, sig_y0, var_0); 
+
+        file << points[i]->func << " + ";
     }
 }
 
@@ -65,8 +77,6 @@ int main() {
     srand(time(NULL));
     ofstream file_surf;
     file_surf.open("func_s.txt");
-    surface off_road(25, 25, 25); //creating new object a in class surface
-    off_road.print_file(file_surf);
-    file_surf.close();
+    surface off_road(file_surf, 25, 25, 25); //creating new object a in class surface
     return 0;
 }
